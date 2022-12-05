896e29106bf9cb0babaa025ccbbf1f5bd7a78207
Bartemix836 <114190309+Bartemix836@users.noreply.github.com>
2022-12-05 14:04:18 +0100

    Add files via upload

---
+++ src/app/Chat.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import '../Assets/chat-style/Chat.css';
+import ChatHeader from './ChatHeader';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import Message from '../Components/Message';
+import { useSelector } from 'react-redux';
+import { selectChannelId, selectChannelName } from '../features/appSlice';
+import { selectUser } from '../features/userSlice';
+import db from "../firebase";
+import firebase from 'firebase/compat/app'; 
+import 'firebase/compat/auth'; 
+import 'firebase/compat/firestore';
+
+export default function Chat() {
+    const user = useSelector(selectUser);
+    const channelId = useSelector(selectChannelId);
+    const channelName = useSelector(selectChannelName);
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        if (channelId) {
+            db.collection("channels")
+                .doc(channelId)
+                .collection("messages")
+                .orderBy("timestamp", "desc")
+                .onSnapshot((snapshot) =>
+                    setMessages(snapshot.docs.map((doc) => doc.data()))
+                );
+        }
+  }, [channelId]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection("channels").doc(channelId).collection("messages").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        user: user,
+    });
+
+    setInput("");
+};
+
+  return (
+    <div className='chat'>
+        <ChatHeader channelName ={channelName} />
+
+        <div className="chat-message">
+            {messages.map((message)=>(
+                <Message
+                key={message.key}
+                timestamp={message.timestamp}
+                message={message.message}
+                user={message.user}
+                />
+            ))}
+        </div>
+        <div className="chat-input">
+            <AddCircleRoundedIcon className='plus-input'  fontSize="medium"/>
+            <form>
+                <input 
+                 value = {input} 
+                 disabled = {!channelId}
+                 onChange = {(e)=> setInput(e.target.value)}
+                 placeholder="Write your message"/>
+                
+                <button
+                disabled={!channelId} 
+                className='chat-btn'
+                type='submit'
+                onClick = {sendMessage}>
+                Send
+                </button>    
+            </form>
+        </div>
+    </div>
+  )
+}
---
+++ src/app/ChatHeader.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../Assets/chatheader-style/chatheader.css';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import BookIcon from '@mui/icons-material/Book';
+
+
+function ChatHeader({channelName}) {
+  const [show, setShow] = React.useState(true)          
+  return (
+    <div className='chat-header'>
+        <div className="chatHeaderLeft">
+            
+            <h3>
+                <span className="chaHeaderName-hash"><BookIcon/></span>
+                {channelName}
+            </h3>
+        </div>
+        <div className="chatHeaderRight">
+
+            <HelpRoundedIcon onClick={()=>setShow(!show)}/>
+             
+            {
+                show? <p></p>:<div className='help-info'> 
+                      <p>It is a platform where you can exchange recipes with others users. 
+                        The platform consist of tree channels where you can divorce a lot of new recipes, 
+                        which are simple and tasteful. 
+                        If you know how to work our app you can close this window by click again "Help" button</p>
+                        </div> 
+            }
+           
+        </div>
+    </div>
+  )
+}
+
+export default ChatHeader
